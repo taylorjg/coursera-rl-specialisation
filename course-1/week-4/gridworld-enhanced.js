e9e0f76f8f031f768020eba214e49e8edc7fac92
taylorjg <jonathan.taylor.programmer@gmail.com>
2021-02-03 19:41:08 +0000

    Move more code into gridworld-utils

--- course-1/week-4/gridworld-enhanced.js
+++ course-1/week-4/gridworld-enhanced.js
@@ -5,74 +5,38 @@ const S = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 const TERMINAL_STATE = 99
 const S_PLUS = [...S, TERMINAL_STATE]
 
-const UP = 0
-const DOWN = 1
-const RIGHT = 2
-const LEFT = 3
-const A = [UP, DOWN, RIGHT, LEFT]
+const TERMINAL_STATE_COORDS = [[0, 0]]
 
 const GAMMA = 1
 const THETA = 2 + 1e-12
 
-const GPI = configureGPI(S, A, GAMMA, THETA)
-
-const newCoordsAfterTakingAction = (x, y, a) => {
-  switch (a) {
-    case UP: return [x, y - 1]
-    case DOWN: return [x, y + 1]
-    case RIGHT: return [x + 1, y]
-    case LEFT: return [x - 1, y]
-    default: throw new Error(`[newCoordsAfterTakingAction] Unknown action "${a}"`)
-  }
-}
-
-const coordsAreOffGrid = (x, y) => (x < 0 || y < 0 || x > 3 || y > 3)
-const coordsAreTerminal = (x, y) => x === 0 && y === 0
-const stateToCoords = s => [s % 4, Math.floor(s / 4)]
-const coordsToState = (x, y) => y * 4 + x
+const GPI = configureGPI(S, GW.A, GAMMA, THETA)
 
 const dynamics = (s, a) => {
   if (s === TERMINAL_STATE) {
     return [{ p: 1, s2: s, r: 0 }]
   }
-  const [x1, y1] = stateToCoords(s)
-  const [x2, y2] = newCoordsAfterTakingAction(x1, y1, a)
-  if (coordsAreOffGrid(x2, y2)) {
+  const [x1, y1] = GW.stateToCoords(s)
+  const [x2, y2] = GW.newCoordsAfterTakingAction(x1, y1, a)
+  if (GW.coordsAreOffGrid(x2, y2)) {
     const r = [1, 2, 3, 8, 9, 10].includes(s) ? -10 : -1
     return [{ p: 1, s2: s, r: r }]
   }
-  const s2 = coordsAreTerminal(x2, y2)
+  const s2 = GW.coordsAreTerminal(TERMINAL_STATE_COORDS, x2, y2)
     ? TERMINAL_STATE
-    : coordsToState(x2, y2)
+    : GW.coordsToState(x2, y2)
   const r = [1, 2, 3, 8, 9, 10].includes(s2) ? -10 : -1
   return [{ p: 1, s2, r }]
 }
 
 const main = () => {
-
   const V = new Map(S_PLUS.map(s => [s, 0]))
   const pi = new Map(S.map(s => [s, 0]))
-
   for (; ;) {
     if (GPI.improvePolicy(V, pi, dynamics)) break
     GPI.evaluatePolicy(V, pi, dynamics)
   }
-
-  console.log(`Optimal policy:`)
-  GW.printMapInGrid(S, TERMINAL_STATE)(pi, a => {
-    switch (a) {
-      case UP: return '\u2191'
-      case DOWN: return '\u2193'
-      case RIGHT: return '\u2192'
-      case LEFT: return '\u2190'
-      default: throw new Error(`[printGrid#formatValue] Unknown action "${a}"`)
-    }
-  })
-
-  console.log()
-
-  console.log(`Optimal state value function:`)
-  GW.printMapInGrid(S, TERMINAL_STATE)(V)
+  GW.printResults(S, TERMINAL_STATE)(V, pi)
 }
 
 main()
--- course-1/week-4/gridworld-utils.js
+++ course-1/week-4/gridworld-utils.js
@@ -1,5 +1,27 @@
 const U = require('../../utils')
 
+const UP = 0
+const DOWN = 1
+const RIGHT = 2
+const LEFT = 3
+const A = [UP, DOWN, RIGHT, LEFT]
+
+const newCoordsAfterTakingAction = (x, y, a) => {
+  switch (a) {
+    case UP: return [x, y - 1]
+    case DOWN: return [x, y + 1]
+    case RIGHT: return [x + 1, y]
+    case LEFT: return [x - 1, y]
+    default: throw new Error(`[newCoordsAfterTakingAction] Unknown action "${a}"`)
+  }
+}
+
+const coordsAreOffGrid = (x, y) => (x < 0 || y < 0 || x > 3 || y > 3)
+const coordsAreTerminal = (TERMINAL_STATE_COORDS, x, y) =>
+  TERMINAL_STATE_COORDS.some(([tscx, tscy]) => x == tscx && y === tscy)
+const stateToCoords = s => [s % 4, Math.floor(s / 4)]
+const coordsToState = (x, y) => y * 4 + x
+
 const printMapInGrid = (S, TERMINAL_STATE) => (map, formatValue = v => v) => {
   const w = 5
   for (const y of U.rangeIter(4)) {
@@ -14,6 +36,32 @@ const printMapInGrid = (S, TERMINAL_STATE) => (map, formatValue = v => v) => {
   }
 }
 
+const printResults = (S, TERMINAL_STATE) => (V, pi) => {
+
+  console.log(`Optimal policy:`)
+  printMapInGrid(S, TERMINAL_STATE)(pi, a => {
+    switch (a) {
+      case UP: return '\u2191'
+      case DOWN: return '\u2193'
+      case RIGHT: return '\u2192'
+      case LEFT: return '\u2190'
+      default: throw new Error(`[printGrid#formatValue] Unknown action "${a}"`)
+    }
+  })
+
+  console.log()
+
+  console.log(`Optimal state value function:`)
+  printMapInGrid(S, TERMINAL_STATE)(V)
+}
+
 module.exports = {
-  printMapInGrid
+  UP, DOWN, LEFT, RIGHT,
+  A,
+  newCoordsAfterTakingAction,
+  coordsAreOffGrid,
+  coordsAreTerminal,
+  stateToCoords,
+  coordsToState,
+  printResults
 }
