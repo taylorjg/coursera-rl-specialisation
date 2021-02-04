229efa1d2427235d387fdfa1e680f7470bdbae26
taylorjg <jonathan.taylor.programmer@gmail.com>
2021-02-04 09:50:10 +0000

    Rename a couple of source files to something shorter

--- course-1/week-4/gridworld-enhanced-value-iteration.js
+++ course-1/week-4/gridworld-enhanced-value-iteration.js
@@ -1,5 +1,5 @@
-const GW = require('./gridworld-utils')
-const configureGPI = require('./generalised-policy-iteration')
+const GW = require('./gw')
+const configureGPI = require('./gpi')
 
 const S = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 const S_PLUS = [...S, GW.TERMINAL_STATE]
