f579fe19ab50b00ad7fde5c4c4288ae92f634e47
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2021-02-13 21:02:21 +0000

    Add MC runs to the rms plots

--- course-2/week-2/random-walk-rms.js
+++ course-2/week-2/random-walk-rms.js
@@ -52,6 +52,8 @@ const takeAction = (s, a) => {
   }
 }
 
+// -------------------- TD0 --------------------
+
 const TD0 = (pi, alpha, episodes, cb) => {
   const V = new Map(S_PLUS.map(s => [s, isTerminalState(s) ? 0 : 0.5]))
   for (const _ of U.rangeIter(episodes)) {
@@ -70,6 +72,39 @@ const TD0 = (pi, alpha, episodes, cb) => {
   }
 }
 
+// -------------------- MC --------------------
+
+const generateEpisode = pi => {
+  const episode = []
+  let s = S0
+  for (; ;) {
+    const a = pi(s)
+    const { s2, r, done } = takeAction(s, a)
+    episode.push({ s, a, r })
+    if (done) break
+    s = s2
+  }
+  return episode
+}
+
+const MC = (pi, alpha, episodes, cb) => {
+  const V = new Map(S_PLUS.map(s => [s, isTerminalState(s) ? 0 : 0.5]))
+  for (const _ of U.rangeIter(episodes)) {
+    const episode = generateEpisode(pi)
+    let G = 0
+    const ts = U.range(episode.length)
+    for (const t of ts.reverse()) {
+      const { s, r } = episode[t]
+      G = GAMMA * G + r
+      const v = V.get(s) + alpha * (G - V.get(s))
+      V.set(s, v)
+    }
+    cb(V)
+  }
+}
+
+// -------------------- runs over given prediction method --------------------
+
 const rms = (V1, V2) => {
   const v1 = S.map(s => {
     const v1 = V1.get(s)
@@ -81,20 +116,20 @@ const rms = (V1, V2) => {
   return v3
 }
 
-const doRun = (alpha, episodes) => {
+const doRun = (predictionMethod, alpha, episodes) => {
   const pi = _s => U.randomChoice(A)
   const values = []
   const trueValues = new Map(S.map(s => [s, s / 6]))
-  TD0(pi, alpha, episodes, V => { values.push(rms(V, trueValues)) })
+  predictionMethod(pi, alpha, episodes, V => { values.push(rms(V, trueValues)) })
   return values
 }
 
-const doRuns = (alpha, runs) => {
+const doRuns = (predictionMethod, alpha, runs) => {
   const EPISODES = 100
   const runningAverages = Array(EPISODES).fill(0)
   const counts = Array(EPISODES).fill(0)
   for (const run of U.rangeIter(runs)) {
-    const values = doRun(alpha, EPISODES)
+    const values = doRun(predictionMethod, alpha, EPISODES)
     values.forEach((value, index) => {
       const currentAverage = runningAverages[index]
       const oldCount = counts[index]
@@ -106,11 +141,13 @@ const doRuns = (alpha, runs) => {
   return runningAverages
 }
 
-const makeLine = (values, name, color) => ({
+// -------------------- plot helpers --------------------
+
+const makeLine = (values, name, color, additionalLineAttributes) => ({
   x: values.map((_, index) => index + 1),
   y: values,
   mode: 'lines',
-  line: { color },
+  line: { color, ...additionalLineAttributes },
   name
 })
 
@@ -121,15 +158,29 @@ const makeAnnotation = (x, line) => ({
   showarrow: false
 })
 
+// -------------------- main --------------------
+
 const main = () => {
+
   const RUNS = 100
-  const values1 = doRuns(0.1, RUNS)
-  const values2 = doRuns(0.15, RUNS)
-  const values3 = doRuns(0.05, RUNS)
-  const line1 = makeLine(values1, 'α=0.1', 'cyan')
-  const line2 = makeLine(values2, 'α=0.15', 'cyan')
-  const line3 = makeLine(values3, 'α=0.05', 'cyan')
-  const data = [line1, line2, line3]
+
+  const td0Values1 = doRuns(TD0, 0.1, RUNS)
+  const td0Values2 = doRuns(TD0, 0.15, RUNS)
+  const td0Values3 = doRuns(TD0, 0.05, RUNS)
+  const line1 = makeLine(td0Values1, 'α=0.1', 'cyan')
+  const line2 = makeLine(td0Values2, 'α=0.15', 'cyan')
+  const line3 = makeLine(td0Values3, 'α=0.05', 'cyan')
+
+  const mcValues1 = doRuns(MC, 0.01, RUNS)
+  const mcValues2 = doRuns(MC, 0.02, RUNS)
+  const mcValues3 = doRuns(MC, 0.03, RUNS)
+  const mcValues4 = doRuns(MC, 0.04, RUNS)
+  const line4 = makeLine(mcValues1, 'α=0.01', 'red', { width: 0.5 })
+  const line5 = makeLine(mcValues2, 'α=0.02', 'red', { dash: 'dash' })
+  const line6 = makeLine(mcValues3, 'α=0.03', 'red', { dash: 'dot' })
+  const line7 = makeLine(mcValues4, 'α=0.04', 'red')
+
+  const data = [line1, line2, line3, line4, line5, line6, line7]
   const layout = {
     width: 800,
     height: 600,
@@ -137,7 +188,11 @@ const main = () => {
     annotations: [
       makeAnnotation(70, line1),
       makeAnnotation(75, line2),
-      makeAnnotation(80, line3)
+      makeAnnotation(80, line3),
+      makeAnnotation(20, line4),
+      makeAnnotation(40, line5),
+      makeAnnotation(60, line6),
+      makeAnnotation(80, line7)
     ]
   }
   plot(data, layout)
